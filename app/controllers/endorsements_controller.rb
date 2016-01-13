77e6e42f2de86d53d20d6bd7c6b4daa9022f3a83
octavian rotari <octavianrotari@octavians-mbp.home>
2016-01-13 20:42:08 +0000

    fixed endorsment test

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -3,6 +3,5 @@ class EndorsementsController < ApplicationController
     @review = Review.find(params[:review_id])
     @review.endorsements.create
     render json: {new_endorsement_count: @review.endorsements.count}
-    byebug
   end
 end
